0d820ec2fe90bb1a3ab797b18ec1b33b79b8bfc9
echo <dev.shijun@outlook.com>
2020-03-20 11:13:12 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="main">
+    <div class="header">
+      <div class="info">
+        <a
+          href="/"
+          class="avatar"
+          style="background-image: url(http://dev.pano360.cn/decb5ee755ef93acf71cd907b4d2cfegag.jpg)"
+        ></a>
+        <div class="author">
+          Echo
+        </div>
+      </div>
+      <div class="nav">
+        <span class="fa fa-bars"></span>
+        <a href="" class="search"><span class="fa fa-search"></span></a>
+      </div>
+    </div>
+    <aside class="side animated bounceInLeft" :class="{ active: !isExpand }">
+      <div v-show="!isExpand" class="shrink">
+        <a
+          href="/"
+          class="avatar"
+          style="background-image: url(http://dev.pano360.cn/decb5ee755ef93acf71cd907b4d2cfegag.jpg)"
+        ></a>
+        <span class="fa fa-bars"></span>
+        <a href="" class="search"><span class="fa fa-search"></span></a>
+      </div>
+      <div v-show="isExpand" class="expand">
+        <div class="about">
+          <p class="author">Utone</p>
+          <a
+            href="/"
+            class="avatar"
+            style="background-image: url(http://dev.pano360.cn/decb5ee755ef93acf71cd907b4d2cfegag.jpg)"
+          ></a>
+        </div>
+        <div class="nav">
+          <a href="/" class="current">首页</a>
+          <a href="/">归档</a>
+          <a class="search"><span class="fa fa-search"></span></a>
+        </div>
+        <div class="bottom">
+          <div class="follow">
+            <a href=""><span class="fa fa-github"></span></a>
+            <a href=""><span class="fa fa-rss"></span></a>
+          </div>
+        </div>
+      </div>
+    </aside>
+    <nuxt />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Default extends Vue {
+  private isExpand = true
+  mounted() {
+    this.$nextTick(() => {
+      const doc: HTMLDocument = document
+      doc.onwheel = (e) => {
+        this.isExpand = !(e.deltaY > 0)
+      }
+    })
+  }
+}
+</script>
+<style>
+.main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,207 @@
+<template>
+  <div class="container">
+    <main class="animated bounceInRight">
+      <div class="posts">
+        <div class="post">
+          <div class="banner">
+            <a
+              href=""
+              class="image"
+              :style="{
+                backgroundImage: `url(http://dev.pano360.cn/ef29a0e839af75d547e05499478b1es.png)`
+              }"
+            ></a>
+          </div>
+          <div class="describe">
+            <div class="title">
+              <a href="">主体使用</a>
+            </div>
+            <div class="date">
+              <span class="fa fa-clock-o"></span>
+              <span>2019-10-11</span>
+            </div>
+            <div class="meta">
+              <div class="left">
+                <div class="wrap">
+                  <span class="fa fa-folder-o"></span>
+                  <div class="des">
+                    <a href="">Usage</a>
+                  </div>
+                </div>
+                <div class="wrap">
+                  <span class="fa fa-folder-o"></span>
+                  <div class="des">
+                    <a href="">Usage</a>
+                  </div>
+                </div>
+              </div>
+              <div class="right"><a href="">Read More</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="post">
+          <div class="banner">
+            <a
+              href=""
+              class="image"
+              :style="{
+                backgroundImage: `url(http://dev.pano360.cn/ef29a0e839af75d547e05499478b1es.png)`
+              }"
+            ></a>
+          </div>
+          <div class="describe">
+            <div class="title">
+              <a href="">主体使用</a>
+            </div>
+            <div class="date">
+              <span class="fa fa-clock-o"></span>
+              <span>2019-10-11</span>
+            </div>
+            <div class="meta">
+              <div class="left">
+                <div class="wrap">
+                  <span class="fa fa-folder-o"></span>
+                  <div class="des">
+                    <a href="">Usage</a>
+                  </div>
+                </div>
+                <div class="wrap">
+                  <span class="fa fa-folder-o"></span>
+                  <div class="des">
+                    <a href="">Usage</a>
+                  </div>
+                </div>
+              </div>
+              <div class="right"><a href="">Read More</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+<style lang="less">
+.container {
+  display: flex;
+  justify-content: center;
+  padding: 0.3rem;
+  overflow: hidden;
+  width: 100%;
+  position: absolute;
+  box-sizing: border-box;
+  main {
+    max-width: 700px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 2rem;
+    width: 100%;
+    box-sizing: border-box;
+    .posts {
+      .post {
+        display: flex;
+        flex-direction: column;
+        min-height: 10rem;
+        border-radius: 0.3rem;
+        margin-bottom: 2rem;
+        transition: all 0.3s;
+        border: 1px solid #e1e4e8;
+        .banner {
+          width: 100%;
+          height: 20rem;
+          overflow: hidden;
+          border-bottom: 1px solid #f7f7f7;
+          .image {
+            width: 100%;
+            height: 100%;
+            display: inline-block;
+            background-size: cover;
+            background-position: center center;
+            background-color: #fff;
+            transition: all 0.3s ease-out;
+          }
+        }
+        .describe {
+          flex: 1;
+          flex-direction: column;
+          padding: 1rem;
+          font-size: 0.8rem;
+          .title {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            a {
+              font-size: 1.5rem;
+              &:hover {
+                color: #1976d2;
+              }
+            }
+          }
+          .date {
+            color: #7a7a7a;
+            margin: 0.7rem 0;
+            display: flex;
+          }
+          .meta {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            align-items: center;
+            .left {
+              display: flex;
+              flex: 1;
+              .wrap {
+                display: flex;
+                margin: 0.3rem 0.3rem 0.3rem 0;
+                .des {
+                  overflow: hidden;
+                  text-overflow: ellipsis;
+                  white-space: nowrap;
+                  a {
+                    color: #999;
+                    padding-right: 0.3rem;
+                    &:hover {
+                      color: #1976d2;
+                    }
+                  }
+                }
+              }
+            }
+            .right {
+              width: 40%;
+              display: flex;
+              justify-content: flex-end;
+              margin: 0.3rem 0;
+              a {
+                color: #333;
+                display: inline-block;
+                padding: 0.7rem;
+                text-align: center;
+                border-radius: 0.7rem;
+                font-weight: 600;
+                transition: all 0.3s;
+                background-color: #fed336;
+                &:hover {
+                  color: white;
+                  transform: scale(1.1, 1.1);
+                }
+              }
+            }
+          }
+          .fa {
+            color: #7a7a7a;
+            font-size: 1.2rem;
+            margin-top: -1px;
+            padding: 0 0.2rem;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
